d81cdc5c7a7f3c322d99bf9f6bc305e20bb09349
kate li <kateli24@outlook.com>
2019-05-15 21:20:35 +1200

    Restructured project

--- http-prac/src/containers/Blog/Blog.js
+++ http-prac/src/containers/Blog/Blog.js
@@ -1,57 +1,32 @@
 import React, { Component } from "react";
-
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
-import axios from "axios";
 import "./Blog.css";
+import Posts from "../Blog/Posts/Posts";
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    postId: null,
-    loadedData: null
-  };
-  //Ajax calls
-  componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
-      const postList = response.data.slice(0, 4);
-      this.setState({
-        posts: postList
-      });
-    });
-  }
-
-  //when button is clicked, state.postId got id info
-  clickPostHandler = id => {
-    this.setState({
-      postId: id
-    });
-  };
-
- 
+  
 
   render() {
-    const posts = this.state.posts.map(post => {
-      return (
-        <Post
-          key={post.id}
-          title={post.title}
-          author="Kate"
-          clickPost={() => this.clickPostHandler(post.id)}
-        />
-      );
-    });
+    
     return (
-      <div>
-        <section className="Posts"> {posts} </section>{" "}
-        <section>
-          <FullPost id={this.state.postId} loadedData={this.state.loadedData} />{" "}
-        </section>{" "}
-        <section>
-          <NewPost />
-        </section>{" "}
-      </div>
+      
+          <div className="Blog">
+            <header>
+              <nav>
+                <ul>
+                  <li>
+                    <a href=" ">Home</a>
+                  </li>
+                  <li>
+                    <a href=" ">New Post</a>
+                  </li>
+                </ul>
+              </nav>
+            </header>
+            <Posts />
+          </div>
+        
+        
+      
     );
   }
 }
---
+++ http-prac/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import Post from "../../../components/Post/Post";
+import FullPost from "../FullPost/FullPost";
+import NewPost from "../NewPost/NewPost";
+import axios from "axios";
+import "./Post.css";
+
+class Posts extends Component {
+    state = {
+        posts: [],
+        postId: null,
+        loadedData: null
+      };
+      //Ajax calls
+      componentDidMount() {
+        axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
+          const postList = response.data.slice(0, 4);
+          this.setState({
+            posts: postList
+          });
+        });
+      }
+    
+      //when button is clicked, state.postId got id info
+      clickPostHandler = id => {
+        this.setState({
+          postId: id
+        });
+      };
+  render() {
+    const posts = this.state.posts.map(post => {
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author="Kate"
+          clickPost={() => this.clickPostHandler(post.id)}
+        />
+      );
+    });
+    return (
+      <div>
+        <section className="Posts"> {posts} </section>{" "}
+        {/* <section>
+          <FullPost id={this.state.postId} loadedData={this.state.loadedData} />{" "}
+        </section>{" "}
+
+        <section>
+          <NewPost />
+        </section>{" "} */}
+        
+      </div>
+    );
+  }
+}
+export default Posts;
