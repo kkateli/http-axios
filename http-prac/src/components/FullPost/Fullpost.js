b3f69a527a7183e5985c78094f1336973f69d848
kate li <kateli24@outlook.com>
2019-05-15 17:41:20 +1200

    Fixed the bug Closes #1.

--- http-prac/src/components/FullPost/Fullpost.js
+++ http-prac/src/components/FullPost/Fullpost.js
@@ -1,29 +1,44 @@
 import React, { Component } from "react";
-import axios from "axios";
+ import axios from "axios";
 
 import "./FullPost.css";
 
 class FullPost extends Component {
-  
-  
+  state = {
+    loadedPost: null
+}
+  componentDidUpdate() {
+    if (this.props.id) {
+        if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id!==this.props.id)) 
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+        .then(response => {
+          this.setState({
+            loadedPost: response.data
+          });
+        });
+    }
+  } 
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
     if (this.props.id) {
       post = <p>Loading...</p>;
     }
-    if (this.props.loadedData) {
-        console.log(this.props.loadedData);
-      post = (
-        <div className="FullPost">
-          <h1>{this.props.loadedData.title}</h1>
-          <p>{this.props.loadedData.body}</p>
-          <div className="Edit">
-            <button className="Delete">Delete</button>
-          </div>
-        </div>
-      );
-    }
 
+      if (this.state.loadedPost) {
+        console.log(this.state.loadedPost); //works
+        post = (
+          <div className="FullPost">
+            <h1>{this.state.loadedPost.title}</h1>
+            <p>{this.state.loadedPost.body}</p>
+            <div className="Edit">
+              <button className="Delete">Delete</button>
+            </div>
+          </div>
+        );
+      }
+      
+    
     return post;
   }
 }
--- http-prac/src/containers/Blog/Blog.js
+++ http-prac/src/containers/Blog/Blog.js
@@ -7,67 +7,53 @@ import axios from "axios";
 import "./Blog.css";
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        postId: null,
-        loadedData: null
-    };
-    //Ajax calls
-    componentDidMount() {
-        axios
-            .get("https://jsonplaceholder.typicode.com/posts")
-            .then(response => {
-                const postList = response.data.slice(0, 4);
-                this.setState({
-                    posts: postList
-                });
-            });
-    }
+  state = {
+    posts: [],
+    postId: null,
+    loadedData: null
+  };
+  //Ajax calls
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
+      const postList = response.data.slice(0, 4);
+      this.setState({
+        posts: postList
+      });
+    });
+  }
 
-    clickPostHandler = id => {
-        this.setState({
-            postId: id
-        });
-        if (this.state.postId) {
-            axios
-                .get(
-                    "https://jsonplaceholder.typicode.com/posts/" +
-                        this.state.postId
-                )
-                .then(response => {
-                    this.setState({
-                        loadedData: response.data
-                    });
-               });
-        }
-    };
+  //when button is clicked, state.postId got id info
+  clickPostHandler = id => {
+    this.setState({
+      postId: id
+    });
+  };
 
-    render() {
-        const posts = this.state.posts.map(post => {
-            return (
-                <Post
-                    key={post.id}
-                    title={post.title}
-                    author="Kate"
-                    clickPost={() => this.clickPostHandler(post.id)}
-                />
-            );
-        });
-        return (
-            <div>
-                <section className="Posts"> {posts} </section>{" "}
-                <section>
-                    <FullPost
-                        id={this.state.postId}
-                        loadedData={this.state.loadedData}
-                    />{" "}
-                </section>{" "}
-                <section>
-                    <NewPost />
-                </section>{" "}
-            </div>
-        );
-    }
+ 
+
+  render() {
+    const posts = this.state.posts.map(post => {
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author="Kate"
+          clickPost={() => this.clickPostHandler(post.id)}
+        />
+      );
+    });
+    return (
+      <div>
+        <section className="Posts"> {posts} </section>{" "}
+        <section>
+          <FullPost id={this.state.postId} loadedData={this.state.loadedData} />{" "}
+        </section>{" "}
+        <section>
+          <NewPost />
+        </section>{" "}
+      </div>
+    );
+  }
 }
 
 export default Blog;
