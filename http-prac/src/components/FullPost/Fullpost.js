69317ba022667f084a0b2a72c4e6ae25f76f2c21
kate li <kateli24@outlook.com>
2019-05-15 18:26:44 +1200

    Made delete button work

--- http-prac/src/components/FullPost/Fullpost.js
+++ http-prac/src/components/FullPost/Fullpost.js
@@ -1,44 +1,66 @@
 import React, { Component } from "react";
- import axios from "axios";
+import axios from "axios";
 
 import "./FullPost.css";
 
 class FullPost extends Component {
   state = {
     loadedPost: null
-}
+  };
   componentDidUpdate() {
     if (this.props.id) {
-        if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id!==this.props.id)) 
-      axios
-        .get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
-        .then(response => {
-          this.setState({
-            loadedPost: response.data
+      if (
+        !this.state.loadedPost ||
+        (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+      )
+        axios
+          .get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+          .then(response => {
+            this.setState({
+              loadedPost: response.data
+            });
           });
+    }
+  }
+
+  clickDeleteHandler = (id) => {
+    if (id) {
+      axios
+        .delete(
+          "https://jsonplaceholder.typicode.com/posts/" +
+            this.state.loadedPost.id
+        )
+        .then(function(response) {
+          // handle success
+          console.log(response);
+        })
+        .catch(function(error) {
+          // handle error
+          console.log(error);
         });
     }
-  } 
+  };
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
     if (this.props.id) {
       post = <p>Loading...</p>;
     }
 
-      if (this.state.loadedPost) {
-        console.log(this.state.loadedPost); //works
-        post = (
-          <div className="FullPost">
-            <h1>{this.state.loadedPost.title}</h1>
-            <p>{this.state.loadedPost.body}</p>
-            <div className="Edit">
-              <button className="Delete">Delete</button>
-            </div>
+    if (this.state.loadedPost) {
+      console.log(this.state.loadedPost); //works
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.body}</p>
+          <div className="Edit">
+            <button className="Delete" onClick={()=>this.clickDeleteHandler(this.state.loadedPost.id)}>
+              Delete
+            </button>
           </div>
-        );
-      }
-      
-    
+        </div>
+      );
+    }
+
     return post;
   }
 }
