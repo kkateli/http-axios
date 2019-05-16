769a0cccb10788caa25c9e6ad27e4175e270f86d
kate li <kateli24@outlook.com>
2019-05-16 12:33:07 +1200

    Bug fixed Closes #2.

--- http-prac/src/containers/Blog/Blog.js
+++ http-prac/src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./Blog.css";
 import Posts from "../Blog/Posts/Posts";
-import {Route, NavLink} from "react-router-dom";
+import {Route, NavLink,Switch} from "react-router-dom";
 import FullPost from "../Blog/FullPost/FullPost";
 import NewPost from "../Blog/NewPost/NewPost";
 
@@ -31,11 +31,16 @@ class Blog extends Component {
             </header>
             
             {/* <Route path = "/new-post" exact render = {()=><h1>Home1</h1>}/> */}
+            <Switch>
             <Route path="/" exact component={Posts}  />
             
             <Route path="/new-post" exact component ={NewPost} />
             {/* NOTE  */}
+            {/* Also routers are rendered when pathes are matched, use Switch to make routers only render once(when the first time path matches )*/}
             <Route path="/:id" exact component = {FullPost}/>
+
+            </Switch>
+            
 
           </div>
         
